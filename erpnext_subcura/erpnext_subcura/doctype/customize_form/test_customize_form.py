3b99133bc2c5788c6f956eceb57f72792d5d3364
subcura <gordon.zimm@subcura.com>
2025-03-24 10:55:46 +0000

    add doctype Vertrag

---
+++ erpnext_subcura/erpnext_subcura/doctype/customize_form/test_customize_form.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, subcura and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestCustomizeForm(FrappeTestCase):
+	pass
---
+++ erpnext_subcura/erpnext_subcura/doctype/vertrag/vertrag.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, subcura and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Vertrag", {
+// 	refresh(frm) {
+
+// 	},
+// });
