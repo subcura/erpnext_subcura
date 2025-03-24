1324a6d151e5e838585ac373020ea8820c1b6668
subcura <gordon.zimm@subcura.com>
2025-03-24 12:05:13 +0000

    delete custom doctype

--- erpnext_subcura/erpnext_subcura/doctype/customize_form/test_customize_form.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, subcura and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestCustomizeForm(FrappeTestCase):
-	pass
